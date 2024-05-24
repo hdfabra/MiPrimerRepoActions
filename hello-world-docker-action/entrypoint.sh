6baee4347c78be29b40fc63864e6e15fb60e7b6c
hdfabra <41131433+hdfabra@users.noreply.github.com>
2024-05-24 10:18:54 -0500

    Create entrypoint.sh [skip ci]

---
+++ hello-world-docker-action/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Write a message to the console
+echo "Hello, world!"
+
+# Set an output parameter (you can replace "output_value" with your desired value)
+output_value="Some output value"
+
+# Now you can use the variable 'output_value' in your script
+# For example, echo it to the console:
+echo "Output value: \$output_value"
