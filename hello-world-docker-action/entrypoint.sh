4139468341d0473667d022e67710b066e3045a00
hdfabra <41131433+hdfabra@users.noreply.github.com>
2024-05-24 10:35:02 -0500

    Update entrypoint.sh [skip c]

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
+echo "hello $1"
 
-# Write a message to the console
-echo "Hello, world!"
-
-# Set an output parameter (you can replace "output_value" with your desired value)
-output_value="Some output value"
-
-# Now you can use the variable 'output_value' in your script
-# For example, echo it to the console:
-echo "Output value: \$output_value"
+echo "time=$(date)" >> $GITHUB_OUTPUT
